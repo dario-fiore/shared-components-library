496e806d8e027206c530850043a264ba2181cef1
Dario Fiore <dario.fiore@live.it>
2020-08-15 15:31:54 +0200

    first commit

---
+++ stencil.config.ts
@@ -0,0 +1,19 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'sky-shared-component-library',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
